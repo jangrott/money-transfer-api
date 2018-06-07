d694ff4859ac5d0ec85f5efc44a0b11efb6ede00
jangrott <tomek.jangrot@gmail.com>
2018-06-07 11:41:14 +0200

    fix issue on ci (5)

--- src/test/java/pl/jangrot/mtransfer/service/InternalTransferServiceConcurrencyTest.java
+++ src/test/java/pl/jangrot/mtransfer/service/InternalTransferServiceConcurrencyTest.java
@@ -20,7 +20,7 @@ import static pl.jangrot.mtransfer.DataGenerator.ClientBuilder.aClient;
 
 public class InternalTransferServiceConcurrencyTest extends AbstractDaoIntegrationTest {
 
-    private static final int NUM_OF_THREADS = 1000;
+    private static final int NUM_OF_THREADS = 4;
 
     private AccountDaoImpl accountDao;
     private TransferService underTest;
@@ -38,11 +38,6 @@ public class InternalTransferServiceConcurrencyTest extends AbstractDaoIntegrati
 
     @After
     public void clearDB() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         cleanDB();
     }
 
@@ -75,10 +70,10 @@ public class InternalTransferServiceConcurrencyTest extends AbstractDaoIntegrati
 
         Executors.newFixedThreadPool(4).invokeAll(tasks);
 
-        assertThat(fromAccount.getBalance()).isEqualTo(BigDecimal.valueOf(1001000.55));
-        assertThat(toAccount.getBalance()).isEqualTo(BigDecimal.valueOf(499000.55));
-        assertThat(accountDao.getAccount(fromAccount.getId()).get().getBalance()).isEqualTo(BigDecimal.valueOf(1001000.55));
-        assertThat(accountDao.getAccount(toAccount.getId()).get().getBalance()).isEqualTo(BigDecimal.valueOf(499000.55));
+        assertThat(fromAccount.getBalance()).isEqualTo(BigDecimal.valueOf(1000004.55));
+        assertThat(toAccount.getBalance()).isEqualTo(BigDecimal.valueOf(499996.55));
+        assertThat(accountDao.getAccount(fromAccount.getId()).get().getBalance()).isEqualTo(BigDecimal.valueOf(1000004.55));
+        assertThat(accountDao.getAccount(toAccount.getId()).get().getBalance()).isEqualTo(BigDecimal.valueOf(499996.55));
     }
 
     private TransferRequest createTransferRequest(Account fromAccount, Account toAccount, float amount) {
